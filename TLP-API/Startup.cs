5fd52492eb55f6b212028dc771d0d1e61aa2f4b9
Christopher Long <chris@thesoftwaresage.com>
2024-11-25 01:04:37 -0500

    fixing the hostJSON problem

--- TLP-API/Startup.cs
+++ TLP-API/Startup.cs
@@ -3,7 +3,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using TLP_API.Services;                         // services like ICosmosDbService
 using TLP_API.Configuration;                    // config classes like CosmosDbConfig
-using Microsoft.Extensions.Logging;
 
 
 public class Program
@@ -19,11 +18,6 @@ public class Program
                 services.AddSingleton<ICosmosDbService, CosmosDbService>();
 
                 // Optionally add more services
-            })
-            .ConfigureLogging(logging =>
-            {
-                logging.AddConsole(); // Enables console logging
-                logging.SetMinimumLevel(LogLevel.Debug); // Adjust log level as needed
             })
             .Build();
 
